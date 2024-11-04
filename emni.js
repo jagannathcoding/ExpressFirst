32b99a0259f080073d6337678be3c4c7528fdd95
jagannathcoding <jagannathdebgunin@gmail.com>
2024-11-04 22:33:19 +0530

    Initial commit

---
+++ emni.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+app.listen(3000);
+
+
+function emni(n)
+{
+    let sum=0;
+    for(let i=1;i<=n;i++)
+    {
+        sum+=i;
+    }
+    return sum;
+    
+}
+
+
+app.get("/", (req, res) => {
+    const n=req.query.n;
+    const result=emni(n);
+    res.send(result+" is emni");
+});
+
+
